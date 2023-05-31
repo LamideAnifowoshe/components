30d2fc7ad2f081b8bb0ba61c39f6f7c343cd09cb
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2023-05-31 20:02:11 +0100

    install react icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
   return (
     <div>
       <div>
-        <Button primary>Click Me!</Button>
+        <Button primary rounded>
+          <GoBell />
+          Click Me!
+        </Button>
       </div>
 
       <div>
-        <Button secondary>Add to Cart</Button>
+        <Button secondary outline>
+          <GoCloudDownload />
+          Add to Cart
+        </Button>
       </div>
 
       <div>
-        <Button success>Buy Me!</Button>
+        <Button success outline>
+          Buy Me!
+        </Button>
       </div>
 
       <div>
-        <Button warning>View Cart</Button>
+        <Button warning rounded>
+          <GoDatabase />
+          View Cart
+        </Button>
       </div>
 
       <div>
-        <Button danger>Shipping Details</Button>
+        <Button danger outline>
+          Shipping Details
+        </Button>
       </div>
     </div>
   );
