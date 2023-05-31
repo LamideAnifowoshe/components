d106975f81d3626c9ba4abeaa5820d8610d15bbd
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2023-05-31 09:45:40 +0100

    create Button component and pass children props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Button from "./Button";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Click Me!</Button>
+      </div>
+
+      <div>
+        <Button>Add to Cart</Button>
+      </div>
+
+      <div>
+        <Button>Buy Me!</Button>
+      </div>
+
+      <div>
+        <Button>View Cart</Button>
+      </div>
+
+      <div>
+        <Button>Shipping Details</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
