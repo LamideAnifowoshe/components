4fa86a3ab0df84cbad46686b635b802c891b7244
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2023-06-18 12:37:14 +0100

    create Dropdown componnent

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items = [
-    {
-      id: "efky1",
-      label: "hey click me",
-      content: "Thanks for clicking me",
-    },
-    {
-      id: "efky2",
-      label: "hey click me 2",
-      content: "Thanks for clicking me 2",
-    },
-    {
-      id: "efky3",
-      label: "hey click me 3",
-      content: "Thanks for clicking me 3",
-    },
-  ];
-
   return (
     <div>
-      <Accordion items={items} />
+      <Dropdown />
     </div>
   );
 }
